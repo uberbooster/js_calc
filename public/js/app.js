12081dc2e89785028c3c24f3a8b51292e1216734
Michael Wohl <mike@gatorriders.com>
2016-02-10 19:06:49 -0500

    fixed a division error and allowed for multiple EQUAL presses

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
   var $display = $('#display');
   var lastOperation = '';
   var clearDisplay = true;
+  var equalPressCount = 0
+  var lastDisplay = 0;
 
 
 
@@ -21,6 +23,7 @@ $(document).ready(function(){
 
 
   function updateDisplay(){ // this only runs if a "number"ed button was clicked.
+    equalPressCount=0;
     var num = $(this).text(); // This captures the value of the button that was clicked
     //  If the variable clearDisplay is true or if what is displayed on
     //  the calculator screen = "0" or "Div/0" or "", then
@@ -36,6 +39,7 @@ $(document).ready(function(){
   }
 
   function divide(){ // This runs when the divide() function is called (i.e. the divide button is clicked)
+    equalPressCount=0;
     lastOperation = $(this).text(); //  Since this only runs if the divide button is clicked, $(this).text() is
                                     //  equal to the string "/", and we assign lastOperation to it (i.e. "/")
                                     //  We do this so we know what the "last" operation was in case the user
@@ -52,7 +56,7 @@ $(document).ready(function(){
               //  The "else" only executes if the "/" button was pressed AFTER another operator was already pressed.
               //  (e.g. (16 + 48) / 8 = 8) Notice the / was the second expression, so calculator.current already
               //  had 16 + 48 (or 64) stored in it.  Here 64 / 8 = 8, so calculator.current will now equal 8.
-      calculator.result = calculator.current / parseFloat($display.text());
+      calculator.current = calculator.current / parseFloat($display.text());
       $display.text(calculator.current);
     }
     clearDisplay=true;  //  This allows the number to not disapear on the display, but sets a flag
@@ -61,6 +65,7 @@ $(document).ready(function(){
   }
 
   function multiply(){
+    equalPressCount=0;
     lastOperation = $(this).text();
     if(calculator.current === 0){
       calculator.current = parseFloat($display.text());
@@ -72,6 +77,7 @@ $(document).ready(function(){
   }
 
   function subtract(){
+    equalPressCount=0;
     lastOperation = $(this).text();
     if(calculator.current === 0){
       calculator.current = parseFloat($display.text());
@@ -82,32 +88,8 @@ $(document).ready(function(){
     clearDisplay=true;
   }
 
-  function equal(){ //  This runs once the equal button is clicked
-
-    if(lastOperation === ''){ //  checking to see if the user has a calculation to perform. If the press
-                              //  8 then "=", the result will be what the user entered as their last number.
-                              // (e.g. 8 = 8, 4 = 4, 234 = 234)
-      calculator.result = parseFloat($display.text());
-    } else if(lastOperation === "/") {
-        calculator.result = calculator.current / parseFloat($display.text());
-        $display.text(calculator.result);
-    } else if(lastOperation === "x"){
-        calculator.result = calculator.current * parseFloat($display.text());
-        $display.text(calculator.result);
-    } else if(lastOperation === "+"){
-        calculator.result = calculator.current + parseFloat($display.text());
-        $display.text(calculator.result);
-    } else if(lastOperation === "-"){
-        calculator.result = calculator.current - parseFloat($display.text());
-        $display.text(calculator.result);
-    } else {
-        $display.text(calculator.result);
-    }
-    calculator.current = 0;
-    clearDisplay=true;
-  }
-
   function plus(){
+    equalPressCount=0;
     lastOperation = $(this).text();
     if(calculator.current === 0){
       calculator.current = parseFloat($display.text());
@@ -117,9 +99,57 @@ $(document).ready(function(){
     }
     clearDisplay=true;
   }
+  function equal(){ //  This runs once the equal button is clicked
+    equalPressCount = equalPressCount + 1;
+    if(lastOperation === ''){ //  checking to see if the user has a calculation to perform. If the press
+                              //  8 then "=", the result will be what the user entered as their last number.
+                              // (e.g. 8 = 8, 4 = 4, 234 = 234)
+      calculator.result = parseFloat($display.text());
+    } else if(lastOperation === "/") {
+        if(equalPressCount === 1){
+          lastDisplay = parseFloat($display.text());
+          calculator.result = calculator.current / lastDisplay;
+          $display.text(calculator.result);
+        }else{
+          calculator.result = calculator.result / lastDisplay;
+          $display.text(calculator.result);
+        }
+    } else if(lastOperation === "x"){
+        if(equalPressCount === 1){
+          lastDisplay = parseFloat($display.text());
+          calculator.result = calculator.current * lastDisplay;
+          $display.text(calculator.result);
+        }else{
+          calculator.result = calculator.result * lastDisplay;
+          $display.text(calculator.result);
+        }
+    } else if(lastOperation === "+"){
+        if(equalPressCount === 1){
+          lastDisplay = parseFloat($display.text());
+          calculator.result = calculator.current + lastDisplay;
+          $display.text(calculator.result);
+        }else{
+          calculator.result = calculator.result + lastDisplay;
+          $display.text(calculator.result);
+        }
+    } else if(lastOperation === "-"){
+        if(equalPressCount === 1){
+          lastDisplay = parseFloat($display.text());
+          calculator.result = calculator.current - lastDisplay;
+          $display.text(calculator.result);
+        }else{
+          calculator.result = calculator.result - lastDisplay;
+          $display.text(calculator.result);
+        }
+    } else {
+        $display.text(calculator.result);
+    }
+    calculator.current = 0;
+    clearDisplay=true;
+  }
 
   function clear(){
-    var num = "0"
+    equalPressCount=0;
     lastOperation = '';
     $display.text(0);
     clearDisplay=true;
@@ -127,6 +157,9 @@ $(document).ready(function(){
 
   function memAdd(){
 
+    calculator.memory = calculator.memory + parseFloat($display.text());
+    calculator.addToMemory = calculator.memory + parseFloat($display.text());
+    clearDisplay=true
   }
 
   function memSub(){
